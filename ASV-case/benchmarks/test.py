86efbc8314cdfbc2f3447c0f22d5f4fc7932806d
Silhouetteeeeeee <1245552419@qq.com>
2022-12-23 13:13:29 +0800

    change

--- ASV-case/benchmarks/test.py
+++ ASV-case/benchmarks/test.py
@@ -1,3 +1,5 @@
+import time
+
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
@@ -9,19 +11,23 @@ class TimeSuite:
             self.d[x] = None
 
     def time_keys(self):
+        time.sleep(1)
         for key in self.d.keys():
             pass
 
     def time_iterkeys(self):
+        time.sleep(1)
         for key in self.d.iterkeys():
             pass
 
     def time_range(self):
+        time.sleep(1)
         d = self.d
         for key in range(100000):
             x = d[key]
 
     def time_xrange(self):
+        time.sleep(1)
         d = self.d
         for key in range(100000):
             x = d[key]
