e1c5feacfd25e3214f9cb4c7a9269de52996ea0d
Silhouetteeeeeee <1245552419@qq.com>
2022-12-24 10:17:32 +0800

    c

--- ASV-case/benchmarks/calculate1.py
+++
@@ -1,3 +0,0 @@
-def f(x):
-    for i in range(x):
-        print(" ")
--- ASV-case/benchmarks/test.py
+++ ASV-case/benchmarks/test.py
@@ -1,5 +1,8 @@
 import time
-import calculate1
+
+def f(x):
+    for i in range(x):
+        print(" ")
 
 class TimeSuite:
     """
@@ -8,19 +11,19 @@ class TimeSuite:
     """
     def setup(self):
         self.d = {}
-        calculate1.f(100)
+        f(100)
 
     def time_keys(self):
-        calculate1.f(1000)
+        f(1000)
 
     def time_iterkeys(self):
-        calculate1.f(10000)
+        f(10000)
 
     def time_range(self):
-        calculate1.f(100000)
+        f(100000)
 
     def time_xrange(self):
-        calculate1.f(1000000)
+        f(1000000)
 
 
 class MemSuite:
