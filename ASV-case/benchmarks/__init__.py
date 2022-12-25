a6b7c719fe0bf94708d8214f7899eb8df73b2892
Silhouetteeeeeee <1245552419@qq.com>
2022-12-25 10:36:51 +0800

    c

--- ASV-case/benchmarks/__init__.py
+++ ASV-case/benchmarks/__init__.py
@@ -1,5 +1,7 @@
 import sys
 import os
+sys.path.append("D:\项目\项目2022-12\ASV-case\benchmarks")
+
 import cal
 
 pass
--- ASV-case/benchmarks/test.py
+++ ASV-case/benchmarks/test.py
@@ -1,4 +1,7 @@
 import time
+import sys
+import os
+sys.path.append("D:\项目\项目2022-12\ASV-case\benchmarks")
 import cal
 class TimeSuite:
     """
